cfdfd6d9639257cf765584a74cf789120e9d1de9
Huynh Ngoc Hau <ngochau1310@gmail.com>
2025-10-18 08:24:29 +0700

    pull main

--- PRM392_FinalProject_ProductSaleApp_Group2/app/src/main/java/com/example/prm392_finalproject_productsaleapp_group2/net/ApiConfig.java
+++ PRM392_FinalProject_ProductSaleApp_Group2/app/src/main/java/com/example/prm392_finalproject_productsaleapp_group2/net/ApiConfig.java
@@ -2,9 +2,9 @@ package com.example.prm392_finalproject_productsaleapp_group2.net;
 
 public final class ApiConfig {
     // For emulator use: http://10.0.2.2:8080
-    // For physical Hậu's device use: http://192.168.1.2:8080
+    // For physical Hậu's device use: http://192.168.1.8:8080
     // For physical huy's device use: http://192.168.1.3:8080
-    public static final String BASE_URL = "http://192.168.137.70:8080"; // Your computer's WiFi IP
+    public static final String BASE_URL = "https://prm392-finalproject-productsaleapp-be.onrender.com"; // Your computer's WiFi IP
 
     private ApiConfig() {}
 
